e03c8ae217ebbb6cf20c5285b713a7a1835e5fa6
Timur Cheberda <t.cheberda@venga.com>
2025-09-17 17:34:02 +0200

    fix: move debug button to top-right corner

--- apps/miniapp/src/pages/ProfilePage.tsx
+++ apps/miniapp/src/pages/ProfilePage.tsx
@@ -405,7 +405,7 @@ export function ProfilePage() {
       
       {/* Debug Button - только для отладки */}
       <motion.div 
-        className="fixed bottom-4 left-4 z-50"
+        className="fixed top-4 right-4 z-50"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 2 }}
